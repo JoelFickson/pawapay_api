e7f6a53320f06b23acfb9cb2de0b531f60a7c089
Joel Fickson <Joelngozo@gmail.com>
2024-03-23 13:07:54 +0100

    chore: export baseService for pawaPay

--- pawapay_node/src/index.ts
+++ pawapay_node/src/index.ts
@@ -4,11 +4,13 @@ import PaymentsPage from "@resources/payments_page";
 import Refunds from "@resources/refunds";
 import Deposits from "@resources/deposits";
 import Payouts from "@resources/payouts";
+import PawapayBaseService from "@utils/PawapayBaseService";
 
 const pawaPaymentsPage = container.resolve(PaymentsPage);
 const pawaPayRefunds = container.resolve(Refunds);
 const pawaPayDeposits = container.resolve(Deposits);
 const pawaPayPayouts = container.resolve(Payouts);
+const pawapayBaseService = container.resolve(PawapayBaseService);
 
 export * from "./types/Payments";
 export * from "./types/Payout";
@@ -17,6 +19,7 @@ export {
   pawaPaymentsPage,
   pawaPayPayouts,
   pawaPayRefunds,
-  pawaPayDeposits
+  pawaPayDeposits,
+  pawapayBaseService
 };
 
