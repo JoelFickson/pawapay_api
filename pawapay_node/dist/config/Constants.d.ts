fdbbff6cfc923bbefe90627b4412249a54352517
Joel Fickson <Joelngozo@gmail.com>
2024-05-17 21:50:05 +0200

    chore: remove dist folder inside pawapay_noe

--- pawapay_node/dist/config/Constants.d.ts
+++
@@ -1,9 +0,0 @@
-export default class Constants {
-    private static readonly _PAWA_PAY_PROD_URL;
-    private static readonly _PAWA_PAY_SANDBOX_URL;
-    static readonly URLs: {
-        [Constants._PAWA_PAY_PROD_URL]: string;
-        [Constants._PAWA_PAY_SANDBOX_URL]: string;
-    };
-}
-//# sourceMappingURL=Constants.d.ts.map
--- pawapay_node/dist/types/pawaPayErrorResponse.d.ts
+++
@@ -1,6 +0,0 @@
-export interface PawaPayNetworkResponse {
-    errorMessage: string;
-    statusCode: number;
-    errorObject: string;
-}
-//# sourceMappingURL=pawaPayErrorResponse.d.ts.map
