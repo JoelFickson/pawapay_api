ae12d2369ba38eb7cf284c1a775d67878a1b30e0
CI User <CI@users.noreply.github.com>
2024-07-16 08:47:33 +0000

    Bump package version [skip ci]

--- pawapay_node/dist/config/networkManager.js
+++ pawapay_node/dist/config/networkManager.js
@@ -20,7 +20,7 @@ let NetworkHandler = class NetworkHandler {
     axiosInstance;
     constructor() {
         const pawaPayJwt = process.env.PAWAPAY_JWT;
-        const environment = process.env.Node_ENV || "development";
+        const environment = process.env.PAWAPAY_ENV || "development";
         const baseURL = environment.toLowerCase() === "production" ?
             Constants_1.default.URLs[Constants_1.default["_PAWA_PAY_PROD_URL"]] :
             Constants_1.default.URLs[Constants_1.default["_PAWA_PAY_SANDBOX_URL"]];
