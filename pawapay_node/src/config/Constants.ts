91fcc846d0dca123387f74fa7ab8701c756c26ab
Joel Fickson <Joelngozo@gmail.com>
2024-07-17 13:51:25 +0200

    chore: fix urls

--- pawapay_node/src/config/Constants.ts
+++ pawapay_node/src/config/Constants.ts
@@ -3,7 +3,7 @@ export default class Constants {
     private static readonly _PAWA_PAY_SANDBOX_URL = Symbol("PAWA_PAY_SANDBOX_URL");
 
     static readonly URLs = {
-        [Constants._PAWA_PAY_PROD_URL]: "https://dashboard.pawapay.cloud",
-        [Constants._PAWA_PAY_SANDBOX_URL]: "https://dashboard.sandbox.pawapay.cloud"
+        [Constants._PAWA_PAY_PROD_URL]: "https://api.pawapay.cloud",
+        [Constants._PAWA_PAY_SANDBOX_URL]: "https://api.sandbox.pawapay.cloud"
     }
 }
